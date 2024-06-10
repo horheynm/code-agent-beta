c61a61968bcf5032980c2cb7b8fb14232c6a0b39
horheynm <george.scratch.dev@gmail.com>
2024-06-10 18:17:52 -0400

    reqs

--- setup.py
+++ setup.py
@@ -1,5 +1,45 @@
 from setuptools import setup, find_packages
-from typing import List
+
+required_packages = [
+    "annotated-types==0.7.0",
+    "anyio==4.4.0",
+    "certifi==2024.6.2",
+    "distro==1.9.0",
+    "h11==0.14.0",
+    "httpcore==1.0.5",
+    "httpx==0.27.0",
+    "idna==3.7",
+    "openai==1.33.0",
+    "pydantic==2.7.3",
+    "pydantic_core==2.18.4",
+    "python-dotenv==1.0.1",
+    "setuptools==70.0.0",
+    "sniffio==1.3.1",
+    "tqdm==4.66.4",
+    "typing_extensions==4.12.2",
+]
+
+extra_reqs = {
+    "dev": [
+        "annotated-types==0.7.0",
+        "anyio==4.4.0",
+        "certifi==2024.6.2",
+        "distro==1.9.0",
+        "h11==0.14.0",
+        "httpcore==1.0.5",
+        "httpx==0.27.0",
+        "idna==3.7",
+        "openai==1.33.0",
+        "pydantic==2.7.3",
+        "pydantic_core==2.18.4",
+        "python-dotenv==1.0.1",
+        "setuptools==70.0.0",
+        "sniffio==1.3.1",
+        "tqdm==4.66.4",
+        "typing_extensions==4.12.2",
+    ]
+}
+
 
 setup(
     name="Simple component generator",
@@ -10,4 +50,6 @@ setup(
     description="LLM agent for simple react component generation",
     package_dir={"": "src"},
     packages=find_packages("src", include=["api"]),
+    install_requires=required_packages,
+    extras_require=extra_reqs,
 )
